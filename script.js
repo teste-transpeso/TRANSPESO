99013e794d9c48e6b874f0911c302bf7358cb551
teste-transpeso <107429593+teste-transpeso@users.noreply.github.com>
2022-07-19 22:32:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function ganhaCor(id){
     let elemento = document.getElementById(id)
+    elemento.style.fontSize = '10pt';
     elemento.style.transition = '0.3s';
     elemento.style.filter = "grayscale(0%)"
-     elemento.setAttribute('width','430px')
+    elemento.style.width = "300px"
+     
      
      console.log(elemento)
 }
 function perdeCor(id){
     let elemento = document.getElementById(id)
+    elemento.style.fontSize = '13pt';
      elemento.style.filter = "grayscale(100%)"
-     elemento.setAttribute('width','400px')
+     elemento.style.width = "270px"
      console.log(elemento)
 }
 
